d8778303169811f7baaf2946c6f2574732ba22a1
gab25 <gabrielesorci.25@gmail.com>
2024-01-26 10:18:13 +0100

    read config

--- home_link/config.py
+++ home_link/config.py
@@ -1,5 +1,13 @@
+import logging
 import yaml
 
 
 class Config:
-    pass
+    def __init__(self) -> None:
+        logging.info("load configurations")
+        self.__read_yaml()
+
+    def __read_yaml(self):
+        with open("config.yml", "r") as file:
+            config_obj = yaml.safe_load(file)
+            print(config_obj)
--- home_link/main.py
+++ home_link/main.py
@@ -1,12 +1,31 @@
+import asyncio
 import logging
+
+from websockets import serve
 
 from home_link.config import Config
 
 
 def main():
-    logging.basicConfig(format="%(asctime)s - %(levelname)s | %(message)s", datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )
     logging.info("start home-link")
-    
+
     config = Config()
 
+    asyncio.run(start_serve())
+
     logging.info("stop home-link")
+
+
+async def start_serve():
+    async with serve(echo, "localhost", 5000):
+        await asyncio.Future()
+
+
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
