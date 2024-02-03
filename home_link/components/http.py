51d679f01401ce5e2c88dd934ba021d35c9b1ff1
gab25 <gabrielesorci.25@gmail.com>
2024-02-03 11:06:32 +0100

    fix

--- home_link/components/http.py
+++ home_link/components/http.py
@@ -4,12 +4,8 @@ from home_link.config import Device
 
 
 class Http(AbstractComponent):
-    DEFAULT_INTERVAL = 10
-
     def __init__(self, device: Device):
         super().__init__(device)
-        if self.interval is None:
-            self.interval = self.DEFAULT_INTERVAL
 
     async def connect(self):
         pass
