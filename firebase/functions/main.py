66ecb23a600aa764cbe00005ebb8e696e43696f4
gab25 <gabrielesorci.25@gmail.com>
2024-02-13 20:18:01 +0100

    exec function with pubsub

--- firebase/functions/main.py
+++ firebase/functions/main.py
@@ -2,12 +2,12 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
+from firebase_functions import pubsub_fn
 from firebase_admin import initialize_app
 
 initialize_app()
 
 
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello Gab!")
+@pubsub_fn.on_message_published(topic="hello")
+def on_message_example(event: pubsub_fn.CloudEvent[pubsub_fn.MessagePublishedData]) -> None:
+    print("hello Gab!")
