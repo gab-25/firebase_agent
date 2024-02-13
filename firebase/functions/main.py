efd12ce91d57c34b7a37a27acd8778482e65164e
gab25 <gabrielesorci.25@gmail.com>
2024-02-13 21:09:18 +0100

    add schedule function example

--- firebase/functions/main.py
+++ firebase/functions/main.py
@@ -2,12 +2,12 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
+from firebase_functions import scheduler_fn
 from firebase_admin import initialize_app
 
 initialize_app()
 
 
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello Gab!")
+@scheduler_fn.on_schedule(schedule="0 * * * *")
+def on_run_example(event: scheduler_fn.ScheduledEvent) -> None:
+    print("Hello Gab!")
