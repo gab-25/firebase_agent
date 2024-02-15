4a9773bae271e946751df2e228446e64c79d3203
gab25 <gabrielesorci.25@gmail.com>
2024-02-15 21:53:34 +0100

    add htpp entity

--- firebase_agent/entity.py
+++ firebase_agent/entity.py
@@ -1,8 +1,28 @@
 import dataclasses
+import enum
+import abc
+
+
+class EntityType(enum.Enum):
+    HTTP = "http"
+
+
+class EntityAggregateType(enum.Enum):
+    DAILY = "daily"
+    HOURLY = "hourly"
+    MINUTE = "minute"
+
+
+class Entity(abc.ABC):
+    pass
+
 
 @dataclasses.dataclass
-class Entity:
+class HttpEntity(Entity):
     name: str
     url: str
+    username: str = None
+    password: str = None
     token: str = None
     value_prop: str = None
+    create_aggregate: EntityAggregateType = None
--- firebase_agent/main.py
+++ firebase_agent/main.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 
 from firebase_agent import run
-from firebase_agent.entity import Entity
+from firebase_agent.entity import Entity, HttpEntity, EntityType
 
 
 def main():
@@ -13,9 +13,13 @@ def main():
     cred = credentials.Certificate(config["GENERAL"].get("firebase_cert"))
     firebase_admin.initialize_app(cred)
 
-    entities: list[Entity] = [
-        Entity(name=section, **config[section]) for section in filter(lambda s: s not in ["GENERAL"], config.sections())
-    ]
+    entities: list[Entity] = []
+    sections_filtered = filter(lambda s: s not in ["GENERAL"], config.sections())
+    for section in sections_filtered:
+        entity_type = config[section]["type"]
+        del config[section]["type"]
+        if entity_type == EntityType.HTTP.value:
+            entities.append(HttpEntity(name=section, **config[section]))
     run(entities)
 
 
