788c41224368916dd3b17b99c4bcaf1afde2da51
Gabriele Sorci <71929275+gab-25@users.noreply.github.com>
2024-02-16 12:50:26 +0100

    Func aggregate (#4)

--- firebase_agent/main.py
+++ firebase_agent/main.py
@@ -1,22 +1,15 @@
-import configparser
 import firebase_admin
 from firebase_admin import credentials
 
 from firebase_agent import run
-from firebase_agent.entity import Entity
+from firebase_agent.config import config_general, config_entities
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read("./config")
-
-    cred = credentials.Certificate(config["GENERAL"].get("firebase_cert"))
+    cred = credentials.Certificate(config_general.get("firebase_cert"))
     firebase_admin.initialize_app(cred)
 
-    entities: list[Entity] = [
-        Entity(name=section, **config[section]) for section in filter(lambda s: s not in ["GENERAL"], config.sections())
-    ]
-    run(entities)
+    run(config_entities)
 
 
 if __name__ == "__main__":
