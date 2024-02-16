6f3a82d597fd7d14a2c14e346412c775e982f255
gab25 <gabrielesorci.25@gmail.com>
2024-02-16 13:05:03 +0100

    fix remove read config from config.py

--- firebase_agent/config.py
+++ firebase_agent/config.py
@@ -1,6 +1,5 @@
 import enum
 import dataclasses
-import configparser
 
 
 class ConfigEntityType(enum.Enum):
@@ -17,28 +16,3 @@ class ConfigEntity:
     token: str = None
     value_prop: str = None
     create_aggregate: bool = False
-
-
-_config = configparser.ConfigParser()
-_config.read("./config")
-
-
-def _get_config_entities() -> list[ConfigEntity]:
-    _config_entities: list[ConfigEntity] = []
-    for section in filter(lambda s: s not in ["GENERAL"], _config.sections()):
-        config_entity = ConfigEntity(
-            type=ConfigEntityType(_config[section].get("type")),
-            name=section,
-            url=_config[section].get("url"),
-            username=_config[section].get("username"),
-            password=_config[section].get("password"),
-            token=_config[section].get("token"),
-            value_prop=_config[section].get("value_prop"),
-            create_aggregate=_config[section].getboolean("create_aggregate"),
-        )
-        _config_entities.append(config_entity)
-    return _config_entities
-
-
-config_general: dict = dict(_config["GENERAL"])
-config_entities: list[ConfigEntity] = _get_config_entities()
